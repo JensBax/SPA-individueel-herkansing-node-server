db4820c11465ed2bba1377d1d76d01d6aa0e296b
JensBax <jensbax98@gmail.com>
2019-11-19 13:45:31 +0100

    environment

--- config/env/env.js
+++ config/env/env.js
@@ -1,33 +1,33 @@
-// var env = {
-//     webPort: process.env.PORT || 3000,
-//     dbHost: process.env.DB_HOST || 'localhost',
-//     dbPort: process.env.DB_PORT || '',
-//     dbUser: process.env.DB_USER || '',
-//     dbPassword: process.env.DB_PASSWORD || '',
-//     dbDatabase: process.env.DB_DATABASE || 'users'
-// }
-
-// var dburl = process.env.NODE_ENV === 'production' ?
-//     'mongodb://' + env.dbUser + ':' + env.dbPassword + '@' + env.dbHost + ':' + env.dbPort + '/' + env.dbDatabase :
-//     'mongodb://localhost/' + env.dbDatabase
-
-// module.exports = {
-//     env: env,
-//     dburl: dburl
-// };
 var env = {
-    webPort: 3000,
-    dbHost: 'localhost',
-    dbPort: '',
-    dbUser: 'JensBax',
-    dbPassword: 'mygames123',
-    dbDatabase: 'mygames',
-    secretkey: 'spaherkansingmygames'
+    webPort: process.env.PORT || 3000,
+    dbHost: process.env.DB_HOST || 'localhost',
+    dbPort: process.env.DB_PORT || '',
+    dbUser: process.env.DB_USER || '',
+    dbPassword: process.env.DB_PASSWORD || '',
+    dbDatabase: process.env.DB_DATABASE || 'users'
 }
 
-var dburl = 'mongodb://' + env.dbUser + ':' + env.dbPassword + '@ds045948.mlab.com:45948/mygames'
+var dburl = process.env.NODE_ENV === 'production' ?
+    'mongodb://' + env.dbUser + ':' + env.dbPassword + '@' + env.dbHost + ':' + env.dbPort + '/' + env.dbDatabase :
+    'mongodb://localhost/' + env.dbDatabase
 
 module.exports = {
     env: env,
     dburl: dburl
 };
+// var env = {
+//     webPort: 3000,
+//     dbHost: 'localhost',
+//     dbPort: '',
+//     dbUser: 'JensBax',
+//     dbPassword: 'mygames123',
+//     dbDatabase: 'mygames',
+//     secretkey: 'spaherkansingmygames'
+// }
+
+// var dburl = 'mongodb://' + env.dbUser + ':' + env.dbPassword + '@ds045948.mlab.com:45948/mygames'
+
+// module.exports = {
+//     env: env,
+//     dburl: dburl
+// };
